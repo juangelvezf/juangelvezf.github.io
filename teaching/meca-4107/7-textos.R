d1605df8d59623f23e3e944cc8603bc39b3e02e1
Taller de R <69440432+taller-R@users.noreply.github.com>
2022-07-19 19:43:42 -0500

    task Angel

--- teaching/meca-4107/7-textos.R
+++ teaching/meca-4107/7-textos.R
@@ -2,10 +2,11 @@
 ## install package
 rm(list=ls())
 require(pacman)
-p_load(rio,tidyverse,sf,leaflet,class)
+p_load(rio,tidyverse,sf,leaflet,class,skimr)
 
 ##=== 0. load data ===##
 house = import("http://eduard-martinez.github.io/data/fill-gis-vars/train.rds")
+skim(house)
 
 ## dataframe to sf
 house = st_as_sf(house,coords=c("lon","lat"),crs=4326)
@@ -14,8 +15,8 @@ house = st_as_sf(house,coords=c("lon","lat"),crs=4326)
 leaflet() %>% addTiles() %>% addCircles(data=house)
 
 ## summary data
-table(is.na(house_mnz$surface_total))
-table(is.na(house_mnz$surface_covered))
+table(is.na(house$surface_total))
+table(is.na(house$surface_covered))
 
 ##=== 1. Extrac text ===##
 
@@ -30,14 +31,14 @@ str_detect(string = word , pattern = "19") ## Detect the presence of a pattern m
 
 str_locate(string = word , pattern = "19") ##  Locate the positions of pattern matches in a string
 
-str_count(string = word , pattern = "19") ## Count the number of matches in a string
+str_count(string = word , pattern = "o") ## Count the number of matches in a string
   
 ## Subset Strings
 str_extract(string = word , pattern = "19") ## Return the first pattern match found in each strin
 
 str_match(string = word , pattern = "19") ## Return the first pattern match found in each string
 
-str_sub(string = word , start = 20, end = 21)
+str_sub(string = word , start = 1, end = 4)
 
 ## Mutate Strings
 str_replace(string = word , pattern = "19" , replacement = "10+9")
@@ -50,6 +51,8 @@ str_to_upper(string = word)
 str_replace_all(string = word , pattern = " " , replacement = "-")
 
 str_replace_all(string = word , "[:blank:]" , replacement = "-")
+
+str_replace_all(string = word , "19|2022" , replacement = "-")
 
 str_replace_all(string = word , "[0-9]" , replacement = "-")
 
@@ -72,10 +75,10 @@ house = house %>%
 table(house$new_surface)
 
 ## another pattern
-x = "[:space:]+[:digit:]+[:space:]+Metros"
+y = "[:space:]+[:digit:]+[:space:]+metros"
 house = house %>% 
         mutate(new_surface = ifelse(is.na(new_surface)==T,
-                                    str_extract(string=house$description , pattern= x),
+                                    str_extract(string=house$description , pattern= y),
                                     new_surface))
 table(house$new_surface)
 
@@ -85,32 +88,33 @@ table(house$new_surface)
 mnz = import("http://eduard-martinez.github.io/data/fill-gis-vars/mnz.rds")
 colnames(mnz)
 
-## spatial join
-house_mnz = st_join(house,mnz)
-colnames(house_mnz)
-
 leaflet() %>% addTiles() %>% addPolygons(data=mnz , color="red") %>% addCircles(data=house)
+
+## spatial join
+house_mnz = st_join(x = house,y = mnz)
+colnames(house_mnz)
 
 ## average block
 house_mnz = house_mnz %>%
             group_by(MANZ_CCNCT) %>%
-            mutate(new_surface=median(surface_total,na.rm=T))
+            mutate(new_surface_2=median(surface_total,na.rm=T))
 
 table(is.na(house_mnz$surface_total))
 
 table(is.na(house_mnz$surface_total),
       is.na(house_mnz$new_surface)) # ahora solo tenemos 221 missing values
 
-## k-vecinos spaciales
-test = is.na(house_mnz$surface_total)
-no_test = is.na(house_mnz$surface_total)==F
+## make buffer
+house_buf = st_buffer(house,dist=20)
 
+leaflet() %>% addTiles() %>% addPolygons(data=house_buf , color="red") %>% addCircles(data=house)
 
-k1 = knn(train=house_mnz[no_test,c("geometry","surface_total")], ## base de entrenamiento
-         test=house_mnz[test,c("geometry","surface_total")],   ## base de testeo
-         cl=house_mnz$surface_total[test], ## outcome
-         k=1)  
+house_buf = st_join(house_buf,house[,"surface_total"])
 
+st_geometry(house_buf) = NULL
+house_buf_mean = house_buf %>% group_by(property_id) %>% summarise(surface_new_3=mean(surface_total.y,na.rm=T))
+
+house_mnz = left_join(house_mnz,house_buf_mean,"property_id")
 
 ##=== 3. CENSO data ===##
 
@@ -118,10 +122,11 @@ k1 = knn(train=house_mnz[no_test,c("geometry","surface_total")], ## base de entr
 mnz_censo = import("http://eduard-martinez.github.io/data/fill-gis-vars/mnz_censo.rds")
 
 ## about data
-browseURL("https://eduard-martinez.github.io/teaching/meca-4107/6-censo.txt")
+browseURL("https://eduard-martinez.github.io/teaching/meca-4107/7-censo.txt")
 
 ## spatial join
 house_censo = st_join(house_mnz,mnz_censo)
 colnames(house_censo)
+
 
 
